b07d95d7ba7e95aa46976cc857aa1d08fe6e7b7e
Domitille Chenut <domitille.de.percin@gmail.com>
2016-08-09 17:19:32 +0200

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  #lire toutes les tasks
+  # get "tasks" => "tasks#index"
+
+  # get "tasks/:id" => "tasks#show"
+
+  # get"tasks/new" => "restaurant#new"
+  # post"restaurants" => "restaurants#create"
+
+  # get"tasks/:id/ edit" => "restaurant#edit"
+  # patch"restaurants/:id" => "restaurants#uptade"
+
+  # delete "restaurants/:id" => "restaurants#destroy"
+
+  resources :tasks
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
